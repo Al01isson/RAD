76a6c57659a2e2f56148f05abf7d0cf9bf4379dd
ALISSON LOPES DO MONTE <202302634151@alunos.estacio.br>
2024-03-06 09:48:57 -0300

    novo

--- main.py
+++ main.py
@@ -69,8 +69,31 @@ except FileNotFoundError as erro:
     print("Arquivo inexistente")
     print("descrição", erro)'''
 
-try:
+'''try:
     f = open("novo2.txt", "r")
     f.write("write")
 except IOError as erro:
+    print("O erro foi", erro)'''
+
+'''notas = [("Carlos", 2.0), ("Lucas", 10.0), ("Maria", 5.5)]
+with open("notas.txt", "w") as f:
+    for nome,nota in Notas:
+        f.write(f"Nome:"(nome), "Nota:"(nota)'\n')
+
+with open("notas.txt")'''
+
+
+
+
+try:
+    with open("teste.txt", "r") as arquivo:
+        print(arquivo.read())
+except FileNotFoundError:
+    print("Arquivo inexistente")
+
+try:
+    f = open("teste2.txt", "r")
+    f.write("write")
+except IOError as erro:
     print("O erro foi", erro)
+    
